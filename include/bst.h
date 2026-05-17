fa07ad323cf20a3145f731f0f15e2d57432de7bb
dariyadmitrievnad-lgtm <dariyadmitrievnad@gmail.com>
2026-05-17 17:23:34 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,143 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+class BST
+{
+ private:
+
+    struct Node
+    {
+        T key;
+        int count;
+
+        Node* left;
+        Node* right;
+
+        Node(const T& value)
+        {
+            key = value;
+            count = 1;
+
+            left = nullptr;
+            right = nullptr;
+        }
+    };
+
+    Node* root;
+
+ private:
+
+    void insert(Node*& node, const T& value)
+    {
+        if (node == nullptr)
+        {
+            node = new Node(value);
+            return;
+        }
+
+        if (value < node->key)
+        {
+            insert(node->left, value);
+        }
+        else if (value > node->key)
+        {
+            insert(node->right, value);
+        }
+        else
+        {
+            node->count++;
+        }
+    }
+
+    bool search(Node* node, const T& value) const
+    {
+        if (node == nullptr)
+            return false;
+
+        if (value == node->key)
+            return true;
+
+        if (value < node->key)
+            return search(node->left, value);
+
+        return search(node->right, value);
+    }
+
+    int depth(Node* node) const
+    {
+        if (node == nullptr)
+            return 0;
+
+        int leftDepth = depth(node->left);
+        int rightDepth = depth(node->right);
+
+        return std::max(leftDepth, rightDepth) + 1;
+    }
+
+    void collect(Node* node,
+                 std::vector<std::pair<T, int>>& data) const
+    {
+        if (node == nullptr)
+            return;
+
+        collect(node->left, data);
+
+        data.push_back({node->key, node->count});
+
+        collect(node->right, data);
+    }
+
+    void clear(Node* node)
+    {
+        if (node == nullptr)
+            return;
+
+        clear(node->left);
+        clear(node->right);
+
+        delete node;
+    }
+
+ public:
+
+    BST()
+    {
+        root = nullptr;
+    }
+
+    ~BST()
+    {
+        clear(root);
+    }
+
+    void insert(const T& value)
+    {
+        insert(root, value);
+    }
+
+    bool search(const T& value) const
+    {
+        return search(root, value);
+    }
+
+    int depth() const
+    {
+        return depth(root);
+    }
+
+    std::vector<std::pair<T, int>> getData() const
+    {
+        std::vector<std::pair<T, int>> data;
+
+        collect(root, data);
+
+        return data;
+    }
+};
+
 #endif  // INCLUDE_BST_H_
